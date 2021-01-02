0afcef8a0f0a0ae8d282a2f629025ed85f9d7c13
poussy <poussy@novelari.com>
2021-01-02 13:54:15 +0200

    add x2 at the legend

--- src/simulator_components/Legend.js
+++ src/simulator_components/Legend.js
@@ -8,9 +8,11 @@ function legend({x1,x2,k1,k2,m1,m2,w,f}){
   var x1 = parseFloat(numerator)/parseFloat(denominator)
   console.log(x1)
   return   (
-    <div style={{textAlign:"left",marginTop:"17vh",marginRight:"20vw"}}>
+    <div style={{textAlign:"left",marginTop:"11vh",marginRight:"20vw"}}>
         <div>X_1/Xst={Math.abs((x1*k1/f).toFixed(4))||"..."}</div> 
         <div>X_1={Math.abs((x1).toFixed(4))||"..."}</div>
+        <div>X_2/Xst={Math.abs((x2*k1/f).toFixed(4))||"..."}</div> 
+        <div>X_2={Math.abs((x2).toFixed(4))||"..."}</div>
         <div>Xst={f/k1||"..."}</div>
         <div>K_1={k1||"..."}</div>
         <div>M_1={m1||"..."}</div>
